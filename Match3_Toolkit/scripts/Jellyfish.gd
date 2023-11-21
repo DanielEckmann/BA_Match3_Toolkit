10818959cd1adfe227e2cf4dd55b20c712d5abb1
Daniel Eckmann <daniel.eckmann2001@googlemail.com>
2023-11-21 11:45:56 +0100

    Jellyfishes now correctly emit signal that they are destroyed that is received by the goalcounter

--- Match3_Toolkit/scripts/Jellyfish.gd
+++ Match3_Toolkit/scripts/Jellyfish.gd
@@ -3,7 +3,7 @@ extends Piece
 signal jellyfish_destroyed()
 
 func _ready():
-	
+	self.jellyfish_destroyed.connect(get_node("/root/game_window/GoalCounter")._on_jellyfish_destroyed)
 	super._ready()
 
 func destroy():
